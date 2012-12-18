0e5ae666c0f72d8f3343759412595cd8ceadd0dd
Alexander Crosby <acrosby@asascience.com>
2012-12-18 17:09:56 -0500

    cleaning up a bit

--- wps/wps_processes.py
+++ wps/wps_processes.py
@@ -3,63 +3,6 @@ from process import process
 from wps.models import StreamGauge
 import xml.etree.ElementTree as et
 
-### Here is a test and example implementation of a wps process
-##class test_process(process):
-##    # These fields are required by the wps descriverprocess and getcapabilities
-##    # requests
-##    title = "Test Process"
-##    abstract = "This is a test process for the sci-wps server."
-##    inputs = [
-##               {"identifier":"value1", #identifier for parameter, same as keyword
-##                                       #for argument in execute method (below)
-##                "abstract":"value1", #abstract for parameter
-##                "title":"value1", #title of parameter
-##                "literal":True, #is literaldata, otherwise complex
-##                "datatype":"float", #datatype
-##                "reference":""}, #ows:Reference or Schema to datatype
-##               {"identifier":"value2",
-##                "abstract":"value2",
-##                "title":"value2",
-##                "literal":True,
-##                "datatype":"float",
-##                "reference":""},
-##               {"identifier":"value3",
-##                "abstract":"value3",
-##                "title":"value3",
-##                "literal":True,
-##                "datatype":"float",
-##                "reference":""},
-##             ]
-##    outputs = [
-##                {"identifier":"OUTPUT",
-##                 "abstract":"Process outputs xml of mutiplication of input values.",
-##                 "datatype":"text/xml",
-##                 "title":"output title",
-##                 "literal":False},
-##              ]
-##    version = "1.0"
-##
-##    def __init__(self):
-##        # Do nothing here
-##        pass
-##
-##    def execute(self, value1, value2, value3):
-##        """
-##        "execute" method is required for each wps process, this is the main
-##        method that is called through the web service.
-##
-##        The names of the input paremeters (other than "self") are the same
-##        names that will be required by the wps datainputs and should be
-##        reflected in the inputs property of the class so that they appear
-##        correctly in the describeprocess request.
-##        """
-##
-##        # All wps processes are required to convert the str representation
-##        # of their input parameters to the correct python type for execution
-##        value1, value2, value3 = float(value1), float(value2), float(value3)
-##
-##        return "<float>"+str(value1 * value2 * value3)+"</float>"
-
 class add_gauge_to_stream(process):
     # These fields are required by the wps descriverprocess and getcapabilities
     # requests
@@ -193,5 +136,5 @@ class find_upstream_gauges(process):
 ##            upstream_segment_ids.append(upstream_segments[i].getchildren()[0].getchildren()[0].text)
             for streamgauge in StreamGauge.objects.filter(river_segment_id=upstream_segments[i].getchildren()[0].getchildren()[0].text):
                 upstream_gauge_ids.append(streamgauge.stream_gauge_id)
-        return str(upstream_gauge_ids)
+        return "gauge ids: " + str(upstream_gauge_ids)
 

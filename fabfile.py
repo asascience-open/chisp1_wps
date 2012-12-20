ff96032535f96c780370002d29acdf7cad76e690
Alexander Crosby <acrosby@asascience.com>
2012-12-20 14:47:06 -0500

    Adding the fabfile for the chisp1_wps project
    	new file:   fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from __future__ import with_statement
+from fabric.api import run, local, cd, settings, lcd, abort, env
+from fabric.contrib.console import confirm
+
+env.hosts = []
+
+def prepare_deploy():
+    with settings(warn_only=True):
+        result = local('git commit -a')
+        result = local('git push origin master')
+
+def test():
+    with settings(warn_only=True):
+        result = local('python manage.py test')
+    if result.failed and not confirm("Tests Failed...Continue?"):
+        abort("Aborting...")
+
+def deploy():
+    test()
+    prepare_deploy()
+
+

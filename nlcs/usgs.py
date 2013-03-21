830e05e3334f28733713c4d88d92d67c98d68820
Alexander <acrosby@acrosby-paegan.(none)>
2013-03-21 14:49:58 -0400

    .

--- nlcs/usgs.py
+++ nlcs/usgs.py
@@ -10,14 +10,6 @@ def parse_sos_GetObservations(xml):
     name = str(xml.getElementsByTagName("gml:name")[0].firstChild.nodeValue)
 
     length = xml.getElementsByTagName("wml2:value").length
-    #print length
-    #for i in range(0,length):
-    #    print i
-    #    timeText = xml.getElementsByTagName("wml2:time")[i].firstChild.nodeValue
-    #    timeText = timeText[:-6]
-    #    time = datetime.datetime.strptime(timeText, '%Y-%m-%dT%H:%M:%S')
-    #    timestamp.append(time)
-    #    value.append(float(xml.getElementsByTagName("wml2:value")[i].firstChild.nodeValue))
     value = [i.firstChild.nodeValue for i in xml.getElementsByTagName("wml2:value")]
     stime = datetime.datetime.strptime
     time = [stime(i.firstChild.nodeValue[:-6], '%Y-%m-%dT%H:%M:%S') for i in xml.getElementsByTagName("wml2:time")]
@@ -33,14 +25,6 @@ def parse_sos_GetObservationsCAN(xml):
     name = str(xml.getElementsByTagName("gml:name")[0].firstChild.nodeValue)
 
     length = xml.getElementsByTagName("wml2:value").length
-    #print length
-    #for i in range(0,length):
-    #    print i
-    #    timeText = xml.getElementsByTagName("wml2:time")[i].firstChild.nodeValue
-    #    timeText = timeText[:-6]
-    #    time = datetime.datetime.strptime(timeText, '%Y-%m-%dT%H:%M:%S')
-    #    timestamp.append(time)
-    #    value.append(float(xml.getElementsByTagName("wml2:value")[i].firstChild.nodeValue))
     value = [i.firstChild.nodeValue for i in xml.getElementsByTagName("wml2:value")]
     stime = datetime.datetime.strptime
     time = [stime(i.firstChild.nodeValue[:-6], '%Y-%m-%dT%H:%M:%S.000Z') for i in xml.getElementsByTagName("wml2:time")]
